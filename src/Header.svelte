e5698fe403be6e99e70c1fd7ecb83cd32ac31573
Jesse Rappaport <jessetherappaport@gmail.com>
2021-03-04 13:16:08 -0500

    refactor styles

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,7 +1,31 @@
+<style>
+  header {
+  	min-height: 100px;
+  	height: 10%;
+  	/* background: linear-gradient(to bottom, #0e3436 50%, rgb(44, 91, 217)); */
+  	/* opacity: 0.5; */
+  }
+
+  .header-svg  {
+    fill: white;
+    margin: auto;
+    font-size: calc(50px + 60 * ((100vw - 300px) / 1000));
+  }
+
+  .svg-container {
+  	margin: auto;
+  	min-width: 360px;
+  }
+
+  .header-text {
+    transform: translateY(2.5vw);
+  }
+</style>
+
 <header class="svg-container-header">
 	<div class="svg-container">
 		<svg class="header-svg" width="100%" height="100" viewBox="0 0 100 100">
-			<text x="50%" y="50%" text-anchor="middle" >pretty avl tree</text>
+			<text class="header-text" x="50%" y="50%" text-anchor="middle" >pretty avl tree</text>
 		</svg>
 	</div>
 </header>
