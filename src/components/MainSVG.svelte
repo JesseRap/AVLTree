636b2fffc32fdc4839e8b4cf9a61b1eeeb5d6d64
Jesse Rappaport <jessetherappaport@gmail.com>
2021-03-21 03:59:10 -0400

    fix

--- src/components/MainSVG.svelte
+++ src/components/MainSVG.svelte
@@ -1,5 +1,4 @@
 <style>
-
   .svg-main__container {
     position: relative;
     flex-grow: 1;
@@ -7,7 +6,6 @@
 </style>
 
 <script>
-  import { onMount } from 'svelte';
   import TreeSVG from './TreeSVG.svg.svelte';
   import AboutSVG from './AboutSVG.svg.svelte';
 </script>
